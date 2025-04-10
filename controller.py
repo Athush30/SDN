f9c2a1e54381efcaeb6ef503a3e56f620facb4b9
Thusajiny Ahilakumaran <ahilakumarant.22@uom.lk>
2025-04-10 22:09:02 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 from pox.core import core
 from pox.lib.packet import ethernet, arp
 from pox.forwarding.l2_learning import LearningSwitch
+from pox.openflow import ofp
 import logging
 
 log = core.getLogger()
@@ -21,6 +22,7 @@ ip_mac_mapping = {}
 class SimpleForwarding(LearningSwitch):
     def __init__(self, connection):
         LearningSwitch.__init__(self, connection, False)
+        self.connection = connection
 
     def _handle_PacketIn(self, event):
         packet = event.parsed
@@ -29,6 +31,9 @@ class SimpleForwarding(LearningSwitch):
 
         if packet.type == ethernet.ARP_TYPE:
             self._handle_arp(packet, event.connection)
+        else:
+            # For other packets, check if a flow exists, otherwise forward normally
+            self._add_flow_entry(packet, event)
 
         # Continue with basic forwarding
         super(SimpleForwarding, self)._handle_PacketIn(event)
@@ -54,6 +59,30 @@ class SimpleForwarding(LearningSwitch):
                 old_mac = ip_mac_mapping[ip]
                 ip_mac_mapping[ip] = current_mac
                 log.warning("Updated: IP %s changed from MAC %s to %s", ip, old_mac, current_mac)
+
+        # Add flow entry for ARP responses (to avoid reprocessing ARP)
+        self._add_flow_entry(packet, connection)
+
+    def _add_flow_entry(self, packet, connection):
+        """Add a flow entry to the switch."""
+        msg = ofp.ofp_flow_mod()
+        msg.priority = 1  # Set a lower priority for general forwarding (ARP and data packets)
+        
+        # Set match criteria
+        match = ofp.ofp_match()
+        match.dl_src = packet.src  # Match source MAC address
+        match.dl_dst = packet.dst  # Match destination MAC address
+        match.dl_type = packet.type  # Match Ethernet type (e.g., ARP, IP)
+
+        # Set actions (forward the packet to the output port)
+        action = ofp.ofp_action_output(port=connection.ofp_port)
+        msg.actions.append(action)
+
+        msg.match = match
+
+        # Add flow entry
+        connection.send(msg)
+        log.info("Flow entry added for packet: src=%s, dst=%s", packet.src, packet.dst)
 
 def list_ip_mac_mappings():
     """List all stored IP-to-MAC mappings."""
